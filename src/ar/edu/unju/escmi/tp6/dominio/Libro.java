427e01115912c74c3e094d935eba1d133392b53a
Valentina Efimov <valentinaefimov0811@gmail.com>
2025-10-21 16:18:50 -0300

    Se crea la clase Libro [Efimov Valentina]

---
+++ src/ar/edu/unju/escmi/tp6/dominio/Libro.java
@@ -0,0 +1,73 @@
+package ar.edu.unju.escmi.tp6.dominio;
+
+public class Libro {
+    private int id;
+    private String autor;
+    private String titulo;
+    private String isbn;
+    private boolean estado;
+
+    /* Constructor por defecto */
+    public Libro() {
+    }
+
+    /* Constructor parametrizado */
+    public Libro(int id, String autor, String titulo, String isbn, boolean estado) {
+        this.id = id;
+        this.autor = autor;
+        this.titulo = titulo;
+        this.isbn = isbn;
+        this.estado = estado;
+    }
+
+    /* Getters y Setters */
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public boolean isEstado() {
+        return estado;
+    }
+
+    public void setEstado(boolean estado) {
+        this.estado = estado;
+    }
+
+    /* Métodos */
+    public void mostrarDatos() {
+        if (estado) {
+            System.out.println("DISPONIBLE");
+        }
+        else {
+            System.out.println("NO DISPONIBLE");
+        }
+    }
+}
