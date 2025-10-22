8a030b1ff10a3ca41d071db6b0a1e124294f0f7e
Valentina Efimov <valentinaefimov0811@gmail.com>
2025-10-21 23:32:09 -0300

    Se crea la clase CollectionUsuario [Efimov Valentina]

---
+++ src/ar/edu/unju/escmi/tp6/collections/CollectionUsuario.java
@@ -0,0 +1,26 @@
+package ar.edu.unju.escmi.tp6.collections;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import ar.edu.unju.escmi.tp6.dominio.Usuario;
+
+public class CollectionUsuario {
+    public static Map<Integer, Usuario> usuarios = new HashMap<>();
+
+    public static void guardarUsuario(Usuario usuario) {
+        usuarios.put(usuario.getId(), usuario);
+    }
+
+    public static Usuario buscarUsuario(int id) {
+        return usuarios.get(id);
+    }
+
+    public static void mostrarUsuarios() {
+        for (Usuario u : usuarios.values()) {
+            u.mostrarDatos();
+            System.out.println("-------------------");
+        }
+    }
+    
+}
